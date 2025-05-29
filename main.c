60c7ab0d0c17302b7d6c43d33e8994b9b3187dff
Ivo Bruno <ivobrunodev@gmail.com>
2025-05-29 14:47:16 -0300

    inclusao do case

--- main.c
+++ main.c
@@ -4,10 +4,35 @@ int main (){
     float num1 = 0.0, num2 = 0.0;
     char operacao = ' ';
 
-    printf("Digite o primeiro numero: ");
-    scanf("%f", &num1);
-    printf("\nDigite o segundo numero: ");
-    scanf("%f", &num2);
-    printf("Digite a operacao desejada (+ - * /)");
-    scanf("%c", &operacao);
+    while (1){
+        printf("Digite o primeiro numero: ");
+        scanf("%f", &num1);
+        printf("\nDigite o segundo numero: ");
+        scanf("%f", &num2);
+        printf("Digite a operacao desejada (+ - * /)");
+        scanf("%c", &operacao);
+
+        if (operacao == '/' && num2 == 0){
+            printf("\nOperacao invalida, divisao por 0 (zero)! ");
+            continue;
+        }
+        
+        switch (operacao){
+            case '+':
+                printf("\n%f + %f = %.2f", num1, num2, num1 + num2);
+                break;
+            case '-':
+                printf("\n%f - %f = %.2f", num1, num2, num1 - num2);
+                break;
+            case '*':
+                printf("\n%f + %f = %.2f", num1, num2, num1 * num2);
+                break;
+            case '/':
+                printf("\n%f + %f = %.2f", num1, num2, num1 / num2);
+                break;
+            default:
+                printf("\nOperacao invalida, tente novamente. ");
+        }
+    }
+    return 0;
 }
