8d35a45e38d24e75fea99b1be65878274936d064
Ivo Bruno <ivobrunodev@gmail.com>
2025-05-29 14:35:07 -0300

    update

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 int main (){
-    
+    float num1 = 0.0, num2 = 0.0;
+    char operacao = ' ';
+
+    printf("Digite o primeiro numero: ");
+    scanf("%f", &num1);
+    printf("\nDigite o segundo numero: ");
+    scanf("%f", &num2);
+    printf("Digite a operacao desejada (+ - * /)");
+    scanf("%c", &operacao);
 }
